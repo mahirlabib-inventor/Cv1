8ea2f1dec0e0e3f91df61af3f5bf93dbfa496656
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-07-24 00:51:27 +0600

    v1

---
+++ C-01_(first program)/comments.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+	/*
+	This
+	is 
+	a 
+	multiple
+	line 
+	comment
+	*/
+
+
+	//this is a single line comment
+
+
+	return 0;
+}
---
+++ C-01_(first program)/hello.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("hello world\n");
+	/*
+	printf() is function which prints something to the scren it prints texts under double quote
+	*/
+	return 0;//return will be discussed later
+}
